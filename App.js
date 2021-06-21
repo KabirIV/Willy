c41506747df14dec9a9bcc0bfe57db80ebb7d646
Kabir <Planeman189@gmail.com>
2021-06-21 11:25:15 -0400

    willy

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import{createBottomTabNavigator}from 'react-navigation-tabs'
+import BookTransactionScreen from './screens/BookTransactionScreen'
+import SearchScreen from './screens/SearchScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
 }
+const TabNavigator=createBottomTabNavigator({
+  Transaction:{screen:BookTransactionScreen},
+  Search:{screen:SearchScreen}
+},
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:()=>{
+      const routeName=navigation.state.routeName
+      if(routeName==="Transaction"){
+        return(
+          <Image source={require("./assets/book.png")}
+          style={{width:40,height:40}}/>
+        )
+      }
+      else if(routeName==="Search"){
+        return(
+          <Image source={require("./assets/searchingbook.png")}
+          style={{width:40,height:40}}/>
+        )
+      }
+    }
+  })
+}
+)
+const AppContainer=createAppContainer(TabNavigator)
